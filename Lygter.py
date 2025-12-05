23da29847e4585b9eb697821de80e76bbc9b4bea
Zerraphix <88774528+Zerraphix@users.noreply.github.com>
2025-12-05 09:55:31 +0100

    Lavet forlygter i baglygte filen, så ændret navnet til Lygter

--- Lygter.py
+++ Lygter.py
@@ -1,8 +1,14 @@
 from machine import Pin, PWM
 import time
+from neopixel import NeoPixel
 import ldr_sensor
 import acceloremeter
 
+#JP1 3v3 til +
+#JP1 12 til DI
+#JP1 GND til GND
+antal = 12
+np = NeoPixel(Pin(12, Pin.OUT), antal)
 LED_PIN = 5
 
 pwm = PWM(Pin(LED_PIN))
@@ -10,7 +16,10 @@ pwm.freq(1000)
 
 
 def mørke_lys(): # normal baglygte (svag)
-    pwm.duty_u16(20000) # ca. 30% (0-65535)
+    pwm.duty_u16(20000) # ca. 30% (0-65535) for baglygte
+    for i in range(antal): # for forlygte
+        np[i] = (255,255,255)
+    np.write()
     print("light on")
 
 def bremse_lys(): # bremse-lys (kraftig)
@@ -18,30 +27,26 @@ def bremse_lys(): # bremse-lys (kraftig)
     print("Brems")
 
 def light_off():
-    pwm.duty_u16(0)
+    pwm.duty_u16(0) # for baglygte
+    for i in range(antal): # for forlygte
+        np[i] = (0,0,0)
+    np.write()
     print("light off")
     
 ldr = "MØRKT"
 last_brake_check = True
 while True:
 
-    if ldr_sensor.DayorNight() != ldr:
-        
-        if ldr_sensor.DayorNight() == "MØRKT":
-            mørke_lys()
-        if ldr_sensor.DayorNight() == "LYS":
-            light_off()
-        ldr = ldr_sensor.DayorNight()
-    if acceloremeter.check_brake() != last_brake_check:
+    if ldr_sensor.DayorNight() != ldr or acceloremeter.check_brake() != last_brake_check:
         if acceloremeter.check_brake():
             bremse_lys()
-            last_brake_check = acceloremeter.check_brake()
         elif ldr_sensor.DayorNight() == "MØRKT":
             mørke_lys()
-            last_brake_check = acceloremeter.check_brake()
         else:
             light_off()
-            last_brake_check = acceloremeter.check_brake()
+        last_brake_check = acceloremeter.check_brake()
+        ldr = ldr_sensor.DayorNight()
+            
     time.sleep(0.5)
 
     
