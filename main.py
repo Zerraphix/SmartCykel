b555f808b9d87d9ad3e4ebf16eae50bdfe2e5b4a
QDS_ <98415462+QDSss@users.noreply.github.com>
2025-12-10 11:21:47 +0100

    Add files via upload
    Ikke done

--- main.py
+++ main.py
@@ -1,75 +1,65 @@
 from time import sleep
 import gc
 import secrets
-from machine import UART
 
-from gps_module import get_lat_lon
 from battery_simple import read_battery
-import time
-import ldr_sensor
-import acceloremeter
-from uthingsboard.client import TBDeviceMqttClient
+from temperature import read_temperature
 from rc522_test import read_tag, tag_is_allowed
+from uthingsboard.client import TBDeviceMqttClient
 
+SERVER = secrets.SERVER_IP_ADDRESS
+TOKEN = secrets.ACCESS_TOKEN
 
-uart = UART(2, 9600)
-
-client = TBDeviceMqttClient(
-    secrets.SERVER_IP_ADDRESS,
-    access_token=secrets.ACCESS_TOKEN
-)
-
-print("Forbinder til ThingsBoard...")
-client.connect()
-print("Connected.")
-
-
-SEND_INTERVAL = 30
+SEND_INTERVAL = 2
 counter = 0
 
-ldr = "MØRKT"
-last_brake_check = True
+
+def send_once(data):
+    try:
+        client = TBDeviceMqttClient(SERVER, access_token=TOKEN)
+        client.connect()
+        client.send_telemetry(data)
+        client.disconnect()
+    except Exception as e:
+        print("TB fejl:", e)
 
 
 while True:
+    try:
+        batt = read_battery()
+        th = read_temperature()
+        tag = read_tag()
+        allowed = tag_is_allowed(tag) if tag else False
 
+        print("BAT:", batt)
+        print("TEMP:", th)
+        print("TAG:", tag, allowed)
 
-    pos = get_lat_lon()
-    battery = read_battery()
-    
-    tag = read_tag()
-    tag_ok = False
-    if tag:
-        tag_ok = tag_is_allowed()
+        if counter >= SEND_INTERVAL:
+            data = {}
 
-    if counter >= SEND_INTERVAL:
-        data = {}
-        data.update(battery)
+            data.update(batt)
 
-        if pos:
-            data["latitude"] = pos[0]
-            data["longitude"] = pos[1]
+            if th["temperature_c"] is not None:
+                data["temperature_c"] = th["temperature_c"]
+            if th["humidity"] is not None:
+                data["humidity"] = th["humidity"]
 
-        if tag:
-            data["rfid_tag"] = tag
-            data["rfid_allowed"] = tag_ok
+            if tag:
+                data["rfid_tag"] = tag
+                data["rfid_allowed"] = allowed
 
-        client.send_telemetry(data)
-        print("SENDT:", data)
+            if data:
+                send_once(data)
 
-        counter = 0
+            counter = 0
 
-        
-    if ldr_sensor.DayorNight() != ldr or acceloremeter.check_brake() != last_brake_check:
-        if acceloremeter.check_brake():
-            bremse_lys()
-        elif ldr_sensor.DayorNight() == "MØRKT":
-            mørke_lys()
-        else:
-            light_off()
-        last_brake_check = acceloremeter.check_brake()
-        ldr = ldr_sensor.DayorNight()
-    
-    sleep(1)
-    counter += 1
-    gc.collect()
+        sleep(1)
+        counter += 1
+        gc.collect()
+
+    except KeyboardInterrupt:
+        break
+    except Exception as e:
+        print("Main fejl:", e)
+        sleep(2)
