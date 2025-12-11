2c70d72d8d103c942e6d79f28d2fdf816b8691f6
QDS_ <mepj04@gmail.com>
2025-12-11 10:41:49 +0100

    Opdatering

--- main.py
+++ main.py
@@ -10,33 +10,31 @@ import alarm_knap_lib
 from gps_module import get_lat_lon
 import ldr_sensor
 import lygter
-import acceloremeter
+#import acceloremeter
 import lcd_display
-
+from machine import UART
+    
 SERVER = secrets.SERVER_IP_ADDRESS
 TOKEN = secrets.ACCESS_TOKEN
 
 SEND_INTERVAL = 5
+
 uart = UART(2, 9600)
 
-client = TBDeviceMqttClient(
-    secrets.SERVER_IP_ADDRESS,
-    access_token=secrets.ACCESS_TOKEN
-)
+# Første ThingsBoard-client (du oprettede to – holder kun den rigtige)
+client = TBDeviceMqttClient(SERVER, access_token=TOKEN)
+client.set_server_side_rpc_request_handler(lock_lib.handle_rpc, alarm_knap_lib.handle_rpc)
 
 print("Forbinder til ThingsBoard...")
 try:
     client.connect()
     print("Connected.")
-except:
-    print("Error connecting")
-    
-counter = 0
-client = TBDeviceMqttClient(SERVER, access_token=TOKEN)
-client.set_server_side_rpc_request_handler(lock_lib.handle_rpc, alarm_knap_lib.handle_rpc)
-client.connect()
-print("Forbundet til ThingsBoard")
+except Exception as e:
+    print("Error connecting:", e)
 
+counter = 0
+"""last_brake_check = False
+ldr = """
 
 while True:
     try:
@@ -48,48 +46,60 @@ while True:
         print("BAT:", batt)
         print("TEMP/HUM:", th)
         print("TAG:", tag, "ALLOWED:", allowed)
+
         if counter >= SEND_INTERVAL:
             data = {}
 
+            # Batteri tilføjes direkte
             data.update(batt)
 
-            if th["temperature_c"] is not None:
+            # Temperatur & luftfugtighed
+            if th.get("temperature_c") is not None:
                 data["temperature_c"] = th["temperature_c"]
-            if th["humidity"] is not None:
+            if th.get("humidity") is not None:
                 data["humidity"] = th["humidity"]
 
+            # RFID
             data["rfid_tag"] = tag if tag else ""
             data["rfid_allowed"] = bool(allowed)
-            
+
+            # GPS
             gps = get_lat_lon()
-    if gps != None:
-        data["latitude"] = gps[0]
-        data["longitude"] = gps[1]
-        data["speed"] = gps[2]
+            if gps:
+                data["latitude"] = gps[0]
+                data["longitude"] = gps[1]
+                data["speed"] = gps[2]
 
             client.send_telemetry(data)
             print("SENDT:", data)
             counter = 0
+
+        # MQTT check
         client.check_msg()
-        
-    lcd_display.lcd_show_status(data)
-        
-    if ldr_sensor.DayorNight() != ldr or acceloremeter.check_brake() != last_brake_check:
-        if acceloremeter.check_brake():
-            lygter.bremse_lys()
-        elif ldr_sensor.DayorNight() == "MØRKT":
-            lygter.mørke_lys()
-        else:
-            lygter.light_off()
-        last_brake_check = acceloremeter.check_brake()
-        ldr = ldr_sensor.DayorNight()
-    
-    sleep(1)
-    counter += 1
-    gc.collect()
-    
-    
-except KeyboardInterrupt:
+
+        # LCD opdatering
+        lcd_display.lcd_show_status(locals().get("data", {}))
+
+        # Sensorbaseret lysstyring
+        """if ldr_sensor.DayorNight() != ldr or acceloremeter.check_brake() != last_brake_check:
+
+            if acceloremeter.check_brake():
+                lygter.bremse_lys()
+
+            elif ldr_sensor.DayorNight() == "MØRKT":
+                lygter.mørke_lys()
+
+            else:
+                lygter.light_off()
+
+        #last_brake_check = acceloremeter.check_brake()
+        ldr = ldr_sensor.DayorNight()"""
+
+        sleep(1)
+        counter += 1
+        gc.collect()
+
+    except KeyboardInterrupt:
         print("Stopper...")
         client.disconnect()
         break
