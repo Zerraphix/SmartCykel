e7191f398230d3babb217e97fbcd55fab6770edb
QDS_ <mepj04@gmail.com>
2025-12-11 10:05:25 +0100

    Opdateret main med TB Knap

--- main.py
+++ main.py
@@ -4,24 +4,20 @@ import secrets
 
 from battery_simple import read_battery
 from temperature import read_temperature
-from rc522_test import read_tag, tag_is_allowed
+from RFIDLæser import read_tag, tag_is_allowed
 from uthingsboard.client import TBDeviceMqttClient
+import lock_lib
+import alarm_knap_lib
 
 SERVER = secrets.SERVER_IP_ADDRESS
 TOKEN = secrets.ACCESS_TOKEN
 
-SEND_INTERVAL = 2
+SEND_INTERVAL = 5
 counter = 0
-
-
-def send_once(data):
-    try:
-        client = TBDeviceMqttClient(SERVER, access_token=TOKEN)
-        client.connect()
-        client.send_telemetry(data)
-        client.disconnect()
-    except Exception as e:
-        print("TB fejl:", e)
+client = TBDeviceMqttClient(SERVER, access_token=TOKEN)
+client.set_server_side_rpc_request_handler(lock_lib.handle_rpc, alarm_knap_lib.handle_rpc)
+client.connect()
+print("Forbundet til ThingsBoard")
 
 
 while True:
@@ -32,9 +28,8 @@ while True:
         allowed = tag_is_allowed(tag) if tag else False
 
         print("BAT:", batt)
-        print("TEMP:", th)
-        print("TAG:", tag, allowed)
-
+        print("TEMP/HUM:", th)
+        print("TAG:", tag, "ALLOWED:", allowed)
         if counter >= SEND_INTERVAL:
             data = {}
 
@@ -45,21 +40,22 @@ while True:
             if th["humidity"] is not None:
                 data["humidity"] = th["humidity"]
 
-            if tag:
-                data["rfid_tag"] = tag
-                data["rfid_allowed"] = allowed
+            data["rfid_tag"] = tag if tag else ""
+            data["rfid_allowed"] = bool(allowed)
 
-            if data:
-                send_once(data)
-
+            client.send_telemetry(data)
+            print("SENDT:", data)
             counter = 0
-
+        client.check_msg()
         sleep(1)
         counter += 1
         gc.collect()
 
     except KeyboardInterrupt:
+        print("Stopper...")
+        client.disconnect()
         break
+
     except Exception as e:
         print("Main fejl:", e)
         sleep(2)
