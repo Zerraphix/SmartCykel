ec6f2d9cef2c717bba2db00299f213ec6335aebe
QDS_ <mepj04@gmail.com>
2025-12-15 11:14:16 +0100

    Rettelser
    Sletning af solenoid_lock,
    Tilføjelse af secrets
    Rettelse i boot

--- boot.py
+++ boot.py
@@ -1,35 +1,33 @@
 import network
-from time import ticks_ms, sleep
+from time import sleep
 import secrets
 
-ssid = secrets.SSID
-password = secrets.PASSWORD
+SSID = secrets.SSID
+PASSWORD = secrets.PASSWORD
+
 
 def do_connect():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
-    
-    wlan.ifconfig((
-        "192.168.2.4",     # IP-adresse til ESP32
-        "255.255.255.0",   # Netmaske
-        "192.168.2.2",     # Gateway
-        "8.8.8.8"          # DNS
-    ))
-
-    print("WiFi: forbinder til:", ssid)
 
     if not wlan.isconnected():
-        wlan.connect(ssid, password)
-        start = ticks_ms()
-        while not wlan.isconnected():
-            sleep(0.5)
-            print(".", end="")
-            if ticks_ms() - start > 10000:
-                print("\nKunne ikke forbinde til WiFi.")
-                return wlan
+        print("WiFi: forbinder til", SSID)
+        try:
+            wlan.connect(SSID, PASSWORD)
+        except OSError as e:
+            print("WiFi fejl ved connect:", e)
+            return wlan
 
-    print("\nForbundet til WiFi!")
-    print("ESP32 IP:", wlan.ifconfig()[0])
+        timeout = 20
+        while not wlan.isconnected() and timeout > 0:
+            print(".", end="")
+            sleep(1)
+            timeout -= 1
+
+    print()
+    print("WiFi connected:", wlan.isconnected())
+    print("ifconfig:", wlan.ifconfig())
     return wlan
+
 
 wlan = do_connect()
